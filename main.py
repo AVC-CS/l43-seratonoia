ae5d68050003246c62d9a949a608851314cd44be
seratonoia <hjizelle@gmail.com>
2024-10-07 00:43:17 -0700

    added input box message

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ def main():
     total = 0
     i = 0
     while i < 5:
-        num = int(input(total))
+        num = int(input('Enter number: '))
         total += num
         i += 1
     print(total)
          
-
+# initialize value to 0 for total then set amoutn of loops for 5
     ########################################
     # Do not delete the return statement
     ########################################
