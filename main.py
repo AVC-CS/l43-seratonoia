e1f7802a1b9fe9e7d22319351c95ebe777b8625b
seratonoia <hjizelle@gmail.com>
2024-10-07 00:40:56 -0700

    notes for rough draft of program

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ def main():
     ########################################
     """
     total = 0
+    i = 0
+    while i < 5:
+        num = int(input(total))
+        total += num
+        i += 1
+    print(total)
+         
 
     ########################################
     # Do not delete the return statement
